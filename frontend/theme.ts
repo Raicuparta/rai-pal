8de3e4eb8367f79f89510165012393ba538673b9
Raicuparta <Raicuparta@users.noreply.github.com>
2023-09-27 12:44:38 +0200

    default badge to light variant

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -3,4 +3,11 @@ import type { MantineThemeOverride } from "@mantine/core";
 export const theme: MantineThemeOverride = {
   defaultRadius: "md",
   primaryColor: "violet",
+  components: {
+    Badge: {
+      defaultProps: {
+        variant: "light",
+      },
+    },
+  },
 };
