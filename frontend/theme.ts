1f298a4dd7739e6b1a5f692abc9f3b52596f9f8b
Raicuparta <Raicuparta@users.noreply.github.com>
2023-09-27 12:39:58 +0200

    remove vanilla-extract

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -1,9 +1,6 @@
 import type { MantineThemeOverride } from "@mantine/core";
-import { themeToVars } from "@mantine/vanilla-extract";
 
 export const theme: MantineThemeOverride = {
   defaultRadius: "md",
   primaryColor: "violet",
 };
-
-export const cssVars = themeToVars(theme);
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 import tsconfig from "./tsconfig.json";
 
@@ -21,7 +20,7 @@ function getAliasesFromTsconfig() {
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react()],
   resolve: {
     alias: getAliasesFromTsconfig(),
   },
