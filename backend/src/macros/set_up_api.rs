e714aaacdce446fcd9b4d996d49648e5e20b57d0
Raicuparta <Raicuparta@users.noreply.github.com>
2023-10-16 13:51:08 +0100

    cleanup

--- backend/src/macros/set_up_api.rs
+++ backend/src/macros/set_up_api.rs
@@ -1,21 +1,23 @@
 #[macro_export]
 macro_rules! set_up_api {
 	( $builder:expr, [ $($func:ident),* $(,)? ] ) => {
-		let types = specta::collect_types![
-				$($func),*
-		];
-		let result = types.map(|types| {
-				#[cfg(debug_assertions)]
-				return tauri_specta::ts::export_with_cfg(
-						types,
-						specta::ts::ExportConfiguration::default()
-								.bigint(specta::ts::BigIntExportBehavior::BigInt),
-						"../frontend/api/bindings.ts",
-				);
-		});
+		#[cfg(debug_assertions)]
+		{
+			let types = specta::collect_types![
+					$($func),*
+			];
+			let result = types.map(|types| {
+					return tauri_specta::ts::export_with_cfg(
+							types,
+							specta::ts::ExportConfiguration::default()
+									.bigint(specta::ts::BigIntExportBehavior::BigInt),
+							"../frontend/api/bindings.ts",
+					);
+			});
 
-		if let Err(specta_err) = result {
+			if let Err(specta_err) = result {
 				println!("Failed to generate TypeScript bindings: {}", specta_err);
+			}
 		}
 
 		$builder.invoke_handler(tauri::generate_handler![$($func),*])
