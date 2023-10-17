2da20a071648865bca27039c79c2c542ac81d599
Raicuparta <Raicuparta@users.noreply.github.com>
2023-10-17 12:35:09 +0100

    handle errors on localstorage read/write

--- frontend/hooks/use-persisted-state.ts
+++ frontend/hooks/use-persisted-state.ts
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 
 export function usePersistedState<TState>(key: string, defaultState: TState) {
 	const [state, setState] = useState<TState>(() => {
-		const persistedState = localStorage.getItem(key);
-		return persistedState !== null ? JSON.parse(persistedState) : defaultState;
+		try {
+			const persistedState = localStorage.getItem(key);
+			return persistedState ? JSON.parse(persistedState) : defaultState;
+		} catch (error) {
+			console.log(
+				`Failed to get localStorage state with key "${key}": ${error}`,
+			);
+			return defaultState;
+		}
 	});
 
 	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(state));
+		if (state) {
+			localStorage.setItem(key, JSON.stringify(state));
+		} else {
+			localStorage.removeItem(key);
+		}
 	}, [state, key]);
 
 	return [state, setState] as const;
