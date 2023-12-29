31683416b794c0ae91c9a6df6db38cc2338c38b3
Raicuparta <Raicuparta@users.noreply.github.com>
2023-12-29 16:05:03 +0100

    show gog owned games (useless)

--- backend/src/providers/gog_provider.rs
+++ backend/src/providers/gog_provider.rs
@@ -1,7 +1,11 @@
+use std::collections::HashSet;
+
 use async_trait::async_trait;
 
 use super::provider::ProviderId;
 use crate::{
+	game_engines::game_engine::GameEngineBrand,
+	game_mode::GameMode,
 	installed_game::InstalledGame,
 	owned_game::OwnedGame,
 	provider::{
@@ -55,6 +59,23 @@ impl ProviderActions for GogProvider {
 	}
 
 	async fn get_owned_games(&self) -> Result<Vec<OwnedGame>> {
-		Ok(Vec::default())
+		Ok(game_scanner::gog::games()
+			.unwrap_or_default()
+			.iter()
+			.map(|game| OwnedGame {
+				// TODO should add a constructor to OwnedGame to avoid ID collisions and stuff.
+				id: game.id.clone(),
+				provider_id: *Self::ID,
+				name: game.name.clone(),
+				installed: false, // TODO
+				os_list: HashSet::default(),
+				// Make engine optional?
+				engine: GameEngineBrand::Unity,
+				release_date: 0,
+				thumbnail_url: String::default(),
+				game_mode: GameMode::Flat,
+				uevr_score: None,
+			})
+			.collect())
 	}
 }
