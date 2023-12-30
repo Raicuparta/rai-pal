9ce918bcd4ae593d8fdb67acc0d02bef0a76e404
Raicuparta <Raicuparta@users.noreply.github.com>
2023-12-30 19:35:11 +0100

    getting xbox game display name

--- backend/src/providers/xbox_provider.rs
+++ backend/src/providers/xbox_provider.rs
@@ -2,7 +2,10 @@ use std::path::PathBuf;
 
 use async_trait::async_trait;
 use winreg::{
-	enums::HKEY_LOCAL_MACHINE,
+	enums::{
+		HKEY_CURRENT_USER,
+		HKEY_LOCAL_MACHINE,
+	},
 	RegKey,
 };
 
@@ -38,49 +41,58 @@ impl ProviderActions for XboxProvider {
 			.open_subkey("SOFTWARE\\Microsoft\\GamingServices")?;
 		let package_roots = gaming_services.open_subkey("PackageRepository\\Root")?;
 		let game_configs = gaming_services.open_subkey("GameConfig")?;
+		let app_packages = RegKey::predef(HKEY_CURRENT_USER).open_subkey("Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\CurrentVersion\\AppModel\\Repository\\Packages")?;
 
 		let mut result = Vec::default();
 
-		for key_result in package_roots.enum_keys() {
-			if let Ok(key) = key_result {
-				if let Ok(child) = package_roots.open_subkey(key) {
-					if let Some(Ok(subkey)) = child.enum_keys().nth(0) {
-						if let Ok(subchild) = child.open_subkey(subkey) {
-							if let Ok(package_id) = subchild.get_value::<String, _>("Package") {
-								if let Ok(root_path) = subchild.get_value::<String, _>("Root") {
-									if let Ok(executables) = game_configs
-										.open_subkey(format!("{}\\Executable", package_id))
+		for key in package_roots.enum_keys().flatten() {
+			if let Ok(child) = package_roots.open_subkey(key) {
+				if let Some(Ok(subkey)) = child.enum_keys().nth(0) {
+					if let Ok(subchild) = child.open_subkey(subkey) {
+						if let Ok(package_id) = subchild.get_value::<String, _>("Package") {
+							if let Ok(root_path) = subchild.get_value::<String, _>("Root") {
+								if let Ok(executables) =
+									game_configs.open_subkey(format!("{package_id}\\Executable"))
+								{
+									if let Some(Ok(first_executable_key)) =
+										executables.enum_keys().nth(0)
 									{
-										if let Some(Ok(first_executable_key)) =
-											executables.enum_keys().nth(0)
+										if let Ok(first_executable) =
+											executables.open_subkey(first_executable_key)
 										{
-											if let Ok(first_executable) =
-												executables.open_subkey(first_executable_key)
+											if let Ok(executable_name) =
+												first_executable.get_value::<String, _>("Name")
 											{
-												if let Ok(executable_name) =
-													first_executable.get_value::<String, _>("Name")
-												{
-													let executable_path = PathBuf::from(root_path)
-														.join(executable_name);
-													println!(
-														"Found xbox game in path {}",
-														executable_path.display().to_string()
-													);
-													if let Ok(game_name) =
+												let executable_path =
+													PathBuf::from(root_path).join(executable_name);
+
+												let display_name = app_packages
+													.open_subkey(&package_id)
+													.and_then(|package| {
+														package
+															.get_value::<String, _>("DisplayName")
+													})
+													.or_else(|error| {
+														eprintln!("Failed to find display name for Xbox game: {}", error);
 														file_name_without_extension(
 															&executable_path,
-														) {
-														if let Some(game) = InstalledGame::new(
-															&executable_path,
-															game_name,
-															*Self::ID,
-															None,
-															None,
-															None,
-														) {
-															result.push(game);
-														}
-													}
+														)
+														.map(ToString::to_string)
+													})
+													.unwrap_or_else(|error| {
+														eprintln!("Failed to get game name from exe path: {}", error);
+														"[Name Not Found]".to_string()
+													});
+
+												if let Some(game) = InstalledGame::new(
+													&executable_path,
+													&display_name,
+													*Self::ID,
+													None,
+													None,
+													None,
+												) {
+													result.push(game);
 												}
 											}
 										}
