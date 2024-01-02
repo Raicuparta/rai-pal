27cb77e7434328d6b01eb4b9d42cacd72a12b705
Raicuparta <Raicuparta@users.noreply.github.com>
2024-01-02 16:08:52 +0100

    add button to open logs folder. closes #109

--- frontend/components/settings/settings-page.tsx
+++ frontend/components/settings/settings-page.tsx
@@ -1,15 +1,32 @@
-import { Button, Stack, Tooltip } from "@mantine/core";
+import { Button, Container, Stack, Tooltip } from "@mantine/core";
 import { resetLocalStorage } from "../../util/local-storage";
+import { IconFolderCode, IconRotateDot } from "@tabler/icons-react";
+import { CommandButton } from "@components/command-button";
+import { openLogsFolder } from "@api/bindings";
 
 export function SettingsPage() {
 	return (
-		<Stack>
-			<Tooltip
-				label="Will reset filters, confirmation dialogs, probably other stuff."
-				position="bottom"
-			>
-				<Button onClick={resetLocalStorage}>Reset settings to defaults</Button>
-			</Tooltip>
-		</Stack>
+		<Container size="xs">
+			<Stack>
+				<CommandButton
+					onClick={openLogsFolder}
+					leftSection={<IconFolderCode />}
+					justify="center"
+				>
+					Open Logs Folder
+				</CommandButton>
+				<Tooltip
+					label="Will reset filters, confirmation dialogs, probably other stuff."
+					position="bottom"
+				>
+					<Button
+						onClick={resetLocalStorage}
+						leftSection={<IconRotateDot />}
+					>
+						Reset settings to defaults
+					</Button>
+				</Tooltip>
+			</Stack>
+		</Container>
 	);
 }
