26c256fce9424c9310ab0f1e836372a38fadc039
Raicuparta <Raicuparta@users.noreply.github.com>
2024-01-06 09:57:46 +0100

    fetch from github.io instead of raw.githubusercontent

--- backend/src/steam/id_lists.rs
+++ backend/src/steam/id_lists.rs
@@ -6,6 +6,8 @@ use std::{
 	future,
 };
 
+use log::error;
+
 use crate::{
 	game_engines::game_engine::GameEngineBrand,
 	serializable_enum,
@@ -13,8 +15,7 @@ use crate::{
 	Result,
 };
 
-const STEAM_APP_IDS_URL_BASE: &str =
-	"https://raw.githubusercontent.com/Raicuparta/steam-app-ids-by-engine/main";
+const STEAM_APP_IDS_URL_BASE: &str = "https://raicuparta.github.io/rai-pal-db/steam-ids";
 
 serializable_enum!(UevrScore { A, B, C, D, E });
 
@@ -24,28 +25,42 @@ serializable_struct!(SteamGame {
 	pub uevr_score: Option<UevrScore>,
 });
 
-async fn get_list(list_name: &str) -> Result<HashSet<String>> {
-	Ok(
-		reqwest::get(format!("{STEAM_APP_IDS_URL_BASE}/{list_name}"))
-			.await?
-			.text()
-			.await?
-			.split('\n')
-			.map(std::string::ToString::to_string)
-			.collect(),
-	)
+async fn get_list(list_name: &str) -> HashSet<String> {
+	match reqwest::get(format!("{STEAM_APP_IDS_URL_BASE}/{list_name}")).await {
+		Ok(response) => match response.text().await {
+			Ok(text) => text
+				.split('\n')
+				.map(|line| line.trim().to_string())
+				.collect(),
+			Err(err) => {
+				error!("Failed to parse ids list {list_name}: {err}");
+				HashSet::default()
+			}
+		},
+		Err(err) => {
+			error!("Failed to download ids list {list_name}: {err}");
+			HashSet::default()
+		}
+	}
 }
 
 // TODO this should be a more generic thing where you can load arbitrary json databases,
 // and show them as columns in Rai Pal.
 // For now it's just a hardcoded uevr db.
-async fn get_uevr_scores() -> Result<HashMap<String, UevrScore>> {
-	Ok(
-		reqwest::get(format!("{STEAM_APP_IDS_URL_BASE}/uevr-scores.json"))
-			.await?
+async fn get_uevr_scores() -> HashMap<String, UevrScore> {
+	match reqwest::get(format!("{STEAM_APP_IDS_URL_BASE}/uevr-scores.json")).await {
+		Ok(response) => response
 			.json::<HashMap<String, UevrScore>>()
-			.await?,
-	)
+			.await
+			.unwrap_or_else(|err| {
+				error!("Failed to parse uevr ids list: {err}");
+				HashMap::default()
+			}),
+		Err(err) => {
+			error!("Failed to download uevr ids list: {err}");
+			HashMap::default()
+		}
+	}
 }
 
 fn get_ids_data_list(ids: &HashSet<String>, engine: GameEngineBrand) -> Vec<SteamGame> {
@@ -62,12 +77,12 @@ pub async fn get() -> Result<HashMap<String, SteamGame>> {
 	let (unity, unreal, godot) =
 		future::join!(get_list("Unity"), get_list("Unreal"), get_list("Godot"),).await;
 
-	let uevr_scores = get_uevr_scores().await.unwrap_or_default();
+	let uevr_scores = get_uevr_scores().await;
 
 	let mut games = [
-		get_ids_data_list(&unity.unwrap_or_default(), GameEngineBrand::Unity),
-		get_ids_data_list(&unreal.unwrap_or_default(), GameEngineBrand::Unreal),
-		get_ids_data_list(&godot.unwrap_or_default(), GameEngineBrand::Godot),
+		get_ids_data_list(&unity, GameEngineBrand::Unity),
+		get_ids_data_list(&unreal, GameEngineBrand::Unreal),
+		get_ids_data_list(&godot, GameEngineBrand::Godot),
 	]
 	.concat();
 
