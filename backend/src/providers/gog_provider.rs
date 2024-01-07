a618f369c78e0bc1fd2ddd11eeefb58473c7d106
Raicuparta <Raicuparta@users.noreply.github.com>
2024-01-07 11:47:18 +0100

    test to get engine from pcgaming wiki

--- backend/src/providers/gog_provider.rs
+++ backend/src/providers/gog_provider.rs
@@ -10,7 +10,7 @@ use crate::{
 		ProviderActions,
 		ProviderStatic,
 	},
-	Result,
+	Result, game_engines::game_engine::GameEngineBrand,
 };
 
 pub struct GogProvider {}
@@ -46,22 +46,26 @@ impl ProviderActions for GogProvider {
 	}
 
 	async fn get_owned_games(&self) -> Result<Vec<OwnedGame>> {
-		Ok(game_scanner::gog::games()
-			.unwrap_or_default()
-			.iter()
-			.map(|game| OwnedGame {
-				// TODO should add a constructor to OwnedGame to avoid ID collisions and stuff.
-				id: game.id.clone(),
-				provider_id: *Self::ID,
-				name: game.name.clone(),
-				installed: false, // TODO
-				os_list: HashSet::default(),
-				engine: None,
+		Ok(futures::future::join_all(game_scanner::gog::games()
+		.unwrap_or_default()
+		.iter()
+		.map(|game| async {
+			let is_unreal = reqwest::get(format!("https://www.pcgamingwiki.com/w/api.php?action=cargoquery&tables=Infobox_game&fields=Engines%2C_pageName%3Dtitle&where=GOGcom_ID%20HOLDS%20%22{}%22&format=json", game.id))
+			.await
+			.map(|response| async { response.text().await.unwrap_or_default() }).unwrap().await.contains("Unity");
+			
+			OwnedGame {
+			// TODO should add a constructor to OwnedGame to avoid ID collisions and stuff.
+			id: game.id.clone(),
+			provider_id: *Self::ID,
+			name: game.name.clone(),
+			installed: false, // TODO
+			os_list: HashSet::default(),
+				engine: if is_unreal { Some(GameEngineBrand::Unity) } else { None },
 				release_date: 0,                  // TODO
 				thumbnail_url: String::default(), // TODO Maybe possible to get from the sqlite db?
 				game_mode: None,
 				uevr_score: None,
-			})
-			.collect())
+			}})).await)
 	}
 }
