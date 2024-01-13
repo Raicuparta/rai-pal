7587a4934c9bd2604f370cdd8643b46560126d08
Raicuparta <Raicuparta@users.noreply.github.com>
2024-01-13 17:12:58 +0100

    actually make sure gog games are owned

--- backend/src/providers/gog_provider.rs
+++ backend/src/providers/gog_provider.rs
@@ -111,7 +111,9 @@ async fn get_engine(gog_id: &str, cache: &provider::EngineCache) -> Option<GameE
 	pc_gaming_wiki::get_engine(&format!("GOGcom_ID%20HOLDS%20%22{gog_id}%22")).await
 }
 
-serializable_struct!(GogDbEntryMeta { release_date: i32 });
+serializable_struct!(GogDbEntryTitle { title: Option<String> });
+serializable_struct!(GogDbEntryImages { square_icon: Option<String> });
+serializable_struct!(GogDbEntryMeta { release_date: Option<i32> });
 
 fn get_database() -> Result<Vec<GogDbEntry>> {
 	// TODO get from registry or something.
@@ -119,36 +121,43 @@ fn get_database() -> Result<Vec<GogDbEntry>> {
 
 	let connection = Connection::open_with_flags(database_path, OpenFlags::SQLITE_OPEN_READ_ONLY)?;
 
-	// TODO: Uh, this is showing me games I don't own, so I should get the manifest IDs from the other table first.
 	let mut statement = connection.prepare(
 		r"SELECT 
-releaseKey,
-MAX(CASE WHEN gamePieceTypeId = 277 THEN value END) AS title,
-MAX(CASE WHEN gamePieceTypeId = 814 THEN value END) AS images,
-MAX(CASE WHEN gamePieceTypeId = 815 THEN value END) AS meta
+    P.id,
+    MAX(CASE WHEN GP.gamePieceTypeId = 277 THEN GP.value END) AS title,
+    MAX(CASE WHEN GP.gamePieceTypeId = 814 THEN GP.value END) AS images,
+    MAX(CASE WHEN GP.gamePieceTypeId = 815 THEN GP.value END) AS meta
 FROM 
-GamePieces
-WHERE
-releaseKey GLOB 'gog_*'
+    Products P
+JOIN 
+    GamePieces GP ON P.id = substr(GP.releaseKey, 5) AND GP.releaseKey GLOB 'gog_*'
 GROUP BY 
-releaseKey;",
+    P.id;",
 	)?;
 
 	let rows: Vec<GogDbEntry> = statement
 		.query_map([], |row| {
-			let id: String = row.get(0)?;
-			let title_json: String = row.get(1)?;
+			let id: i32 = row.get(0)?;
+			let title_json: Option<String> = row.get(1).ok();
 			let images_json: Option<String> = row.get(2).ok();
 			let meta_json: Option<String> = row.get(3).ok();
 
+			let title = title_json
+				.and_then(|json| serde_json::from_str::<GogDbEntryTitle>(&json).ok())
+				.and_then(|title| title.title);
+
 			let release_date = meta_json
 				.and_then(|json| serde_json::from_str::<GogDbEntryMeta>(&json).ok())
-				.map(|meta| meta.release_date);
+				.and_then(|meta| meta.release_date);
+
+			let image_url = images_json
+				.and_then(|json| serde_json::from_str::<GogDbEntryImages>(&json).ok())
+				.and_then(|images| images.square_icon);
 
 			Ok(GogDbEntry {
-				id: id.replace("gog_", ""),
-				title: title_json,
-				image_url: images_json,
+				id: id.to_string(),
+				title: title.unwrap_or_else(|| id.to_string()),
+				image_url,
 				release_date,
 			})
 		})?
