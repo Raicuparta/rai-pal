d5d563c5ba7d3714f35a582ea193aa258fdbc648
Raicuparta <Raicuparta@users.noreply.github.com>
2024-02-09 18:47:05 +0000

    use publish script instead of tauri action

---
+++ publish.ps1
@@ -0,0 +1,62 @@
+param (
+  [Parameter(Mandatory = $true)]
+  [string]$Changelog
+)
+
+function CheckEnvVar {
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]$Var
+  )
+
+  if (-not (Test-Path Env:$Var)) {
+    Write-Error "Environment variable '$Var' is not defined."
+    exit 1
+  }
+}
+
+# CheckEnvVar -Var "TAURI_PRIVATE_KEY"
+# CheckEnvVar -Var "TAURI_KEY_PASSWORD"
+
+# pnpm build
+
+$msiFolder = "./backend/target/release/bundle/msi"
+
+# Get built msi file name.
+$msiName = (Get-ChildItem -Path $msiFolder -Filter "*.msi" | Select-Object -First 1).Name
+
+# Extract version number from file name.
+$version = [regex]::Match($msiName, '.+_(.+)_.+_.+').Groups[1].Value
+
+# Read signature from sig file.
+$signature = Get-Content -Path "$msiFolder/*.sig" -Raw
+
+# Create json that's used by Rai Pal for checking updates.
+$updaterJson = ConvertTo-Json -InputObjec @{
+  version   = "${version}"
+  notes     = "${Changelog}"
+  platforms = @{
+    "windows-x86_64" = @{
+      signature = "${signature}"
+      url       = "https://github.com/Raicuparta/rai-pal/releases/latest/download/updater.zip"
+    }
+  }
+}
+
+$outputFolder = "./publish"
+
+# Recreate publish folder.
+Remove-Item $outputFolder -Force -Recurse -ErrorAction Ignore | Out-Null
+New-Item -ItemType Directory $outputFolder | Out-Null
+
+# Write updater json to latest.json (that's the name Tauri uses).
+$updaterJson | Set-Content -Path "$outputFolder/latest.json"
+
+# Rename files to make it a bit clearer what should be downloaded,
+# and remove version numbers frothe download file. This is important
+# because we want to be able to use GitHub's magic releases/latest links
+# to directly link to the latest release download.
+Copy-Item "$msiFolder/*.msi" "$outputFolder/RaiPal.msi" -Force
+Copy-Item "$msiFolder/*.zip" "$outputFolder/updater.zip" -Force
+
+Write-Output $version
