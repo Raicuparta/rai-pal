ebf560e9882f746004dd944bb67ce749cc89c478
Raicuparta <Raicuparta@users.noreply.github.com>
2024-02-10 14:41:24 +0000

    manually creating update prompt

--- frontend/hooks/use-app-updates.ts
+++ frontend/hooks/use-app-updates.ts
@@ -1,26 +1,60 @@
-import { emit, listen } from "@tauri-apps/api/event";
+import { emit } from "@tauri-apps/api/event";
 import { useEffect } from "react";
+
+import {
+	checkUpdate,
+	installUpdate,
+	onUpdaterEvent,
+} from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
+import { showAppNotification } from "@components/app-notifications";
+import { dialog } from "@tauri-apps/api";
 
 const checkFrequencyMilliseconds = 3.6e6;
 
 export function useAppUpdates() {
 	useEffect(() => {
-		console.log("listen");
-		const unlistenPromise = listen("tauri://update-available", () => {
-			// If an update is available, the update prompt will show,
-			// and we don't want to keep checking for updates,
-			// since that will stack multiple prompts.
-			console.log("update available");
-			clearInterval(interval);
+		const unlistenPromise = onUpdaterEvent(({ error, status }) => {
+			showAppNotification(error || status, error ? "error" : "info");
 		});
 
+		checkUpdate()
+			.then(async ({ shouldUpdate, manifest }) => {
+				if (shouldUpdate) {
+					// You could show a dialog asking the user if they want to install the update here.
+					console.log(
+						`Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`,
+					);
+
+					const shouldUpdate = await dialog.ask("Wanna update this bad boy?", {
+						type: "info",
+						cancelLabel: "No thanks",
+						okLabel: "Update now",
+						title: "Hey",
+					});
+
+					if (!shouldUpdate) {
+						return;
+					}
+
+					// Install the update. This will also restart the app on Windows!
+					await installUpdate();
+
+					// On macOS and Linux you will need to restart the app manually.
+					// You could use this step to display another confirmation dialog.
+					await relaunch();
+				}
+			})
+			.catch((error) => {
+				showAppNotification(error, "error");
+			});
+
 		const interval = setInterval(() => {
-			console.log("update emit");
+			if (!document.hasFocus()) return;
 			emit("tauri://update");
 		}, checkFrequencyMilliseconds);
 
 		return () => {
-			console.log("unlisten");
 			unlistenPromise.then((unlisten) => unlisten());
 			clearInterval(interval);
 		};
