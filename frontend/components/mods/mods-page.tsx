420effc94c5a8b7448f2499a8cd7868e926b583c
Raicuparta <Raicuparta@users.noreply.github.com>
2024-06-15 13:57:13 +0200

    hide deprecated mods in mods tab

--- frontend/components/mods/mods-page.tsx
+++ frontend/components/mods/mods-page.tsx
@@ -9,7 +9,7 @@ import {
 	UnityBackendBadge,
 } from "@components/badges/color-coded-badge";
 import { ModModal } from "./mod-modal";
-import { useUnifiedMods } from "@hooks/use-unified-mods";
+import { UnifiedMod, useUnifiedMods } from "@hooks/use-unified-mods";
 import { ModVersionBadge } from "./mod-version-badge";
 import { ItemName } from "@components/item-name";
 import { getModTitle } from "../../util/game-mod";
@@ -18,12 +18,23 @@ export function ModsPage() {
 	const [selectedModId, setSelectedId] = useState<string>();
 
 	const mods = useUnifiedMods();
+	const filteredMods = useMemo(() => {
+		const result: Record<string, UnifiedMod> = {};
+		for (const [modId, mod] of Object.entries(mods)) {
+			if (!mod.local && mod.remote?.deprecated) {
+				continue;
+			}
+
+			result[modId] = mod;
+		}
+		return result;
+	}, [mods]);
 
 	const selectedMod = useMemo(() => {
-		const result = selectedModId ? mods[selectedModId] : undefined;
+		const result = selectedModId ? filteredMods[selectedModId] : undefined;
 
 		return result;
-	}, [selectedModId, mods]);
+	}, [selectedModId, filteredMods]);
 
 	return (
 		<Stack>
@@ -69,7 +80,7 @@ export function ModsPage() {
 						</Table.Tr>
 					</Table.Thead>
 					<Table.Tbody>
-						{Object.entries(mods).map(([modId, mod]) => (
+						{Object.entries(filteredMods).map(([modId, mod]) => (
 							<Table.Tr
 								key={modId}
 								onClick={() => setSelectedId(mod.common.id)}
