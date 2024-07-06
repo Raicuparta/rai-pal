8c0fab95fce1d6be8c911d230b58c5d32a2ad638
Raicuparta <Raicuparta@users.noreply.github.com>
2024-07-07 01:44:09 +0200

    better performance for frontend state updates

--- frontend/hooks/use-data-partial-subscription.ts
+++ frontend/hooks/use-data-partial-subscription.ts
@@ -1,26 +1,52 @@
 import { useSetAtom, atom } from "jotai";
 import { AppEvent, EventPayload, useAppEvent } from "./use-app-event";
+import { useRef, useEffect } from "react";
 
 export function dataPartialSubscription<TEvent extends AppEvent>(
 	event: TEvent,
 	getId: (payload: EventPayload<TEvent>) => string,
-	defaultValue: Record<string, EventPayload<TEvent>>,
+	defaultValue: Map<string, EventPayload<TEvent>>,
 ) {
-	const stateAtom = atom<Record<string, EventPayload<TEvent>>>(defaultValue);
+	const stateAtom = atom<Map<string, EventPayload<TEvent>>>(defaultValue);
+	const dataMap = new Map<string, EventPayload<TEvent>>();
+
+	stateAtom.write;
 
 	function useDataSubscription() {
 		const setData = useSetAtom(stateAtom);
+		const flushTimeout = useRef<number | undefined>();
 
-		useAppEvent(event, (payload) =>
-			setData((previousData) => {
-				if (!payload) return previousData;
+		useAppEvent(event, (payload) => {
+			if (payload) {
+				dataMap.set(getId(payload), payload);
+				scheduleFlush();
+			}
+		});
 
-				return {
-					...previousData,
-					[getId(payload)]: payload,
-				};
-			}),
-		);
+		const scheduleFlush = useRef(() => {
+			if (!flushTimeout.current) {
+				flushTimeout.current = setTimeout(() => {
+					setData((previousData) => {
+						for (const [key, value] of dataMap) {
+							previousData.set(key, value);
+						}
+						dataMap.clear();
+						return new Map(previousData);
+					});
+					flushTimeout.current = undefined;
+				}, 500);
+			}
+		}).current;
+
+		useEffect(() => {
+			return () => {
+				if (flushTimeout.current) {
+					clearTimeout(flushTimeout.current);
+				}
+			};
+		}, []);
+
+		return stateAtom;
 	}
 
 	return [stateAtom, useDataSubscription] as const;
