9d5de0e1d2df74dd6f08b1c8e05b548e1c17c7be
Raicuparta <Raicuparta@users.noreply.github.com>
2024-07-26 12:01:32 +0200

    avatars

--- frontend/components/thanks/thanks-page.tsx
+++ frontend/components/thanks/thanks-page.tsx
@@ -5,8 +5,10 @@ import {
 	Card,
 	Image,
 	Text,
-	Badge,
 	Box,
+	Avatar,
+	Divider,
+	Indicator,
 } from "@mantine/core";
 import {
 	IconBrandGithubFilled,
@@ -24,148 +26,141 @@ export function ThanksPage() {
 			size="sm"
 		>
 			<Group h="100%">
-				<Stack
+				<Card
 					flex={1}
 					h="100%"
 					style={{ overflowY: "auto" }}
 				>
-					<Stack pb="xs">
-						<Card>
-							<Stack>
-								<Group align="top">
-									<Stack
-										gap={0}
-										bg="dark"
-										justify="center"
-										p="sm"
-										style={{ flex: 1, borderRadius: 10 }}
-									>
-										<Image
-											src="/images/thanks/raicuparta.png"
-											radius="100%"
-										/>
-										<Text
-											fw={500}
-											size="xl"
-											ta="center"
-										>
-											Raicuparta
-										</Text>
-									</Stack>
-									<Stack style={{ flex: 4 }}>
-										<Text>
-											Hello. I made Rai Pal. I also made other VR mods in the
-											past, and am currently working on a universal VR mod for
-											Unity games. If you like what I do, and would like to see
-											more, please consider donating! You can also support me by
-											buying one of my free mods on itch.io.
-										</Text>
-									</Stack>
-								</Group>
-								<Group>
-									<ThanksLinkButton
-										color="blue"
-										href="https://github.com/raicuparta/rai-pal"
-										leftSection={<IconBrandGithubFilled />}
-									>
-										Star Rai Pal on GitHub
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://raicuparta.com"
-										color="grape"
-										leftSection={
-											<Image
-												src="/images/thanks/raicuparta.png"
-												radius="100%"
-												height={20}
-											/>
-										}
-									>
-										raicuparta.com
-									</ThanksLinkButton>
-								</Group>
-								<Group>
-									<ThanksLinkButton
-										href="https://www.patreon.com/raivr"
-										color="pink"
-										leftSection={<IconBrandPatreonFilled />}
-									>
-										Patreon
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://paypal.me/raicuparta/5usd"
-										color="indigo"
-										leftSection={<IconBrandPaypalFilled />}
-									>
-										Paypal
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://raicuparta.itch.io/"
-										color="red"
-										leftSection={<IconBrandItch />}
-									>
-										Itch.io
-									</ThanksLinkButton>
-								</Group>
+					<Stack>
+						<Group align="top">
+							<Stack
+								gap={0}
+								bg="dark"
+								justify="center"
+								p="sm"
+								style={{ flex: 1, borderRadius: 10 }}
+							>
+								<Image
+									src="/images/thanks/raicuparta.png"
+									radius="100%"
+								/>
+								<Text
+									fw={500}
+									size="xl"
+									ta="center"
+								>
+									Raicuparta
+								</Text>
 							</Stack>
-						</Card>
-						<Card>
-							<Stack>
-								<Stack style={{ flex: 4 }}>
-									<Text
-										fw={500}
-										size="xl"
-									>
-										Other modders
-									</Text>
-									<Text>
-										Rai Pal is meant to help you manage game modding, and we
-										can&apos;t do that without the tools that other developers
-										have created. Some of these people don&apos;t have donation
-										links, but I&apos;m extremely grateful for their work.
-									</Text>
-								</Stack>
-								<Stack>
-									<ThanksLinkButton
-										href="https://www.patreon.com/praydog"
-										color="pink"
-										leftSection={<IconBrandPatreonFilled />}
-									>
-										praydog on Patreon
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://github.com/BepInEx"
-										color="blue"
-										leftSection={<IconBrandGithubFilled />}
-									>
-										BepInEx on GitHub
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://www.patreon.com/pardeike"
-										color="pink"
-										leftSection={<IconBrandPatreonFilled />}
-									>
-										Andreas Pardeike on Patreon
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://github.com/sinai-dev"
-										color="blue"
-										leftSection={<IconBrandGithubFilled />}
-									>
-										sinai on GitHub
-									</ThanksLinkButton>
-									<ThanksLinkButton
-										href="https://www.patreon.com/ManlyMarco"
-										color="pink"
-										leftSection={<IconBrandPatreonFilled />}
-									>
-										ManlyMarco on Patreon
-									</ThanksLinkButton>
-								</Stack>
+							<Stack style={{ flex: 4 }}>
+								<Text>
+									Hello. I made Rai Pal. I also made other VR mods in the past,
+									and am currently working on a universal VR mod for Unity
+									games. If you like what I do, and would like to see more,
+									please consider donating! You can also support me by buying
+									one of my free mods on itch.io.
+								</Text>
 							</Stack>
-						</Card>
+						</Group>
+						<Group>
+							<ThanksLinkButton
+								color="blue"
+								href="https://github.com/raicuparta/rai-pal"
+								leftSection={<IconBrandGithubFilled />}
+							>
+								Star Rai Pal on GitHub
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://raicuparta.com"
+								color="grape"
+								leftSection={
+									<Image
+										src="/images/thanks/raicuparta.png"
+										radius="100%"
+										height={20}
+									/>
+								}
+							>
+								raicuparta.com
+							</ThanksLinkButton>
+						</Group>
+						<Group>
+							<ThanksLinkButton
+								href="https://www.patreon.com/raivr"
+								color="pink"
+								leftSection={<IconBrandPatreonFilled />}
+							>
+								Patreon
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://paypal.me/raicuparta/5usd"
+								color="indigo"
+								leftSection={<IconBrandPaypalFilled />}
+							>
+								Paypal
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://raicuparta.itch.io/"
+								color="red"
+								leftSection={<IconBrandItch />}
+							>
+								Itch.io
+							</ThanksLinkButton>
+						</Group>
+						<Divider />
+						<Stack>
+							<Text
+								fw={500}
+								size="xl"
+							>
+								Other modders
+							</Text>
+							<Text>
+								Rai Pal is meant to help you manage game modding, and we
+								can&apos;t do that without the tools that other developers have
+								created. Some of these people don&apos;t have donation links,
+								but I&apos;m extremely grateful for their work.
+							</Text>
+						</Stack>
+						<Stack>
+							<ThanksLinkButton
+								href="https://www.patreon.com/praydog"
+								color="pink"
+								leftSection={<IconBrandPatreonFilled />}
+							>
+								praydog on Patreon
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://github.com/BepInEx"
+								color="blue"
+								leftSection={<IconBrandGithubFilled />}
+							>
+								BepInEx on GitHub
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://www.patreon.com/pardeike"
+								color="pink"
+								leftSection={<IconBrandPatreonFilled />}
+							>
+								Andreas Pardeike on Patreon
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://github.com/sinai-dev"
+								color="blue"
+								leftSection={<IconBrandGithubFilled />}
+							>
+								sinai on GitHub
+							</ThanksLinkButton>
+							<ThanksLinkButton
+								href="https://www.patreon.com/ManlyMarco"
+								color="pink"
+								leftSection={<IconBrandPatreonFilled />}
+							>
+								ManlyMarco on Patreon
+							</ThanksLinkButton>
+						</Stack>
 					</Stack>
-				</Stack>
+				</Card>
 				<Stack
 					h="100%"
 					w={200}
@@ -185,19 +180,36 @@ export function ThanksPage() {
 						style={{ overflowY: "scroll" }}
 					>
 						<Stack>
-							{patrons.activePatrons.map((patron, index) => (
-								<Group key={patron.fullName}>
-									<Badge
+							{patrons.activePatrons.map((patron) => (
+								<Group key={patron.ranking}>
+									{/* <Badge
 										variant="light"
 										color="gray"
 									>
-										{index + 1}
-									</Badge>
+										{patron.ranking}
+									</Badge> */}
+									<Indicator
+										label={patron.ranking}
+										position="top-start"
+										processing={patron.ranking <= 3}
+										withBorder
+										color={
+											patron.ranking <= 3
+												? "red"
+												: patron.ranking <= 10
+													? "blue"
+													: "gray"
+										}
+										size="sm"
+										offset={3}
+									>
+										<Avatar src={patron.imageUrl} />
+									</Indicator>
 									<Box
 										flex={1}
 										style={{ wordBreak: "break-word" }}
 									>
-										{patron.fullName}
+										{patron.name}
 									</Box>
 								</Group>
 							))}
