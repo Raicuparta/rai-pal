390363f75140d41e755aaef6b512726211236668
Raicuparta <Raicuparta@users.noreply.github.com>
2024-07-28 21:23:07 +0200

    actually finally fix the updater

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -554,6 +554,7 @@ fn main() {
 	tauri::Builder::default()
 		.plugin(tauri_plugin_window_state::Builder::default().build())
 		.plugin(tauri_plugin_dialog::init())
+		.plugin(tauri_plugin_updater::Builder::default().build())
 		.plugin(
 			tauri_plugin_log::Builder::new()
 				.level(log::LevelFilter::Info)
