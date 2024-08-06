58130e9b844f95a50da9b013196b4f4d12738adc
Raicuparta <Raicuparta@users.noreply.github.com>
2024-08-06 22:05:53 +0200

    commen

--- frontend/hooks/use-persisted-state.ts
+++ frontend/hooks/use-persisted-state.ts
@@ -5,6 +5,9 @@ import {
 	setLocalStorage,
 } from "../util/local-storage";
 
+// This uses localStorage to persist state.
+// The persisted data will often randomly disappear, but localStorage is really fast to use.
+// So this is good for stuff that is convenient to persist, but not a big deal if it gets lost.
 export function usePersistedState<TState>(defaultState: TState, key?: string) {
 	const [state, setState] = useState<TState>(() => {
 		if (!key) return defaultState;
