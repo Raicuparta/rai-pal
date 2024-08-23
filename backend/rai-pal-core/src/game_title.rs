9d2facc7e635c5b52232923c5cff78558330e81b
Raicuparta <Raicuparta@users.noreply.github.com>
2024-08-23 13:35:56 +0200

    another normalization method

--- backend/rai-pal-core/src/game_title.rs
+++ backend/rai-pal-core/src/game_title.rs
@@ -11,6 +11,7 @@ pub struct GameTitle {
 
 static DEMO_REGEX: &lazy_regex::Lazy<regex::Regex> = regex!(r"(?i)[\((\s+)]demo\)?$");
 static BRACKETS_REGEX: &lazy_regex::Lazy<regex::Regex> = regex!(r"\[.*?\]|\(.*?\)|\{.*?\}|<.*?>");
+static SEPARATORS_REGEX: &lazy_regex::Lazy<regex::Regex> = regex!(r"\s-\s.+");
 
 impl GameTitle {
 	pub fn new(display: &str) -> Self {
@@ -36,6 +37,7 @@ fn get_normalized_titles(title: &str) -> Vec<String> {
 		normalize_title(title),
 		normalize_title(&DEMO_REGEX.replace_all(title, "")),
 		normalize_title(&BRACKETS_REGEX.replace_all(title, "")),
+		normalize_title(&SEPARATORS_REGEX.replace_all(title, "")),
 	];
 
 	let mut seen = HashSet::new();
