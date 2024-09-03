5067c9a8f9be68d38ad206e53433d0d21474e218
Raicuparta <Raicuparta@users.noreply.github.com>
2024-09-03 12:57:03 +0200

    fix error on xbox not found

--- backend/rai-pal-core/src/providers/xbox_provider.rs
+++ backend/rai-pal-core/src/providers/xbox_provider.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{io, path::PathBuf};
 
 use log::error;
 use rai_pal_proc_macros::serializable_struct;
@@ -56,61 +56,79 @@ impl ProviderActions for Xbox {
 		TInstalledCallback: FnMut(InstalledGame) + Send + Sync,
 		TOwnedCallback: FnMut(OwnedGame) + Send + Sync,
 	{
-		#[cfg(target_os = "windows")]
-		{
-			let gaming_services = RegKey::predef(HKEY_LOCAL_MACHINE)
-				.open_subkey("SOFTWARE\\Microsoft\\GamingServices")?;
-			let package_roots = gaming_services.open_subkey("PackageRepository\\Root")?;
-			let game_configs = gaming_services.open_subkey("GameConfig")?;
-			let app_packages = RegKey::predef(HKEY_CURRENT_USER).open_subkey("Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\CurrentVersion\\AppModel\\Repository\\Packages")?;
+		if let Err(error) = find_installed_games(&mut installed_callback) {
+			if error.kind() == std::io::ErrorKind::NotFound {
+				log::info!("Failed to find installed Xbox PC games. This probably means the Xbox PC app isn't installed, or there are no Windows Store games or something. Error: {}", error);
+				return Ok(());
+			}
+		}
 
-			for key in package_roots.enum_keys().flatten() {
-				if let Ok(child) = package_roots.open_subkey(key) {
-					if let Some(Ok(subkey)) = child.enum_keys().nth(0) {
-						if let Ok(subchild) = child.open_subkey(subkey) {
-							if let Ok(package_id) = subchild.get_value::<String, _>("Package") {
-								if let Ok(root_path) = subchild.get_value::<String, _>("Root") {
-									if let Ok(executables) = game_configs
-										.open_subkey(format!("{package_id}\\Executable"))
+		Ok(())
+	}
+}
+
+fn find_installed_games<TInstalledCallback>(
+	mut installed_callback: TInstalledCallback,
+) -> io::Result<()>
+where
+	TInstalledCallback: FnMut(InstalledGame) + Send + Sync,
+{
+	#[cfg(target_os = "windows")]
+	{
+		let gaming_services = RegKey::predef(HKEY_LOCAL_MACHINE)
+			.open_subkey("SOFTWARE\\Microsoft\\GamingServices")?;
+		let package_roots = gaming_services.open_subkey("PackageRepository\\Root")?;
+		let game_configs = gaming_services.open_subkey("GameConfig")?;
+		let app_packages = RegKey::predef(HKEY_CURRENT_USER).open_subkey("Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\CurrentVersion\\AppModel\\Repository\\Packages")?;
+
+		for key in package_roots.enum_keys().flatten() {
+			if let Ok(child) = package_roots.open_subkey(key) {
+				if let Some(Ok(subkey)) = child.enum_keys().next() {
+					if let Ok(subchild) = child.open_subkey(subkey) {
+						if let Ok(package_id) = subchild.get_value::<String, _>("Package") {
+							if let Ok(root_path) = subchild.get_value::<String, _>("Root") {
+								if let Ok(executables) =
+									game_configs.open_subkey(format!("{package_id}\\Executable"))
+								{
+									if let Some(Ok(first_executable_key)) =
+										executables.enum_keys().next()
 									{
-										if let Some(Ok(first_executable_key)) =
-											executables.enum_keys().nth(0)
+										if let Ok(first_executable) =
+											executables.open_subkey(first_executable_key)
 										{
-											if let Ok(first_executable) =
-												executables.open_subkey(first_executable_key)
+											if let Ok(executable_name) =
+												first_executable.get_value::<String, _>("Name")
 											{
-												if let Ok(executable_name) =
-													first_executable.get_value::<String, _>("Name")
-												{
-													let executable_path = PathBuf::from(root_path)
-														.join(executable_name);
+												let executable_path =
+													PathBuf::from(root_path).join(executable_name);
 
-													let display_name = app_packages
-														.open_subkey(&package_id)
-														.and_then(|package| {
-															package.get_value::<String, _>(
-																"DisplayName",
-															)
-														})
-														.or_else(|error| {
-															error!("Failed to find display name for Xbox game: {}", error);
-															file_name_without_extension(
-																&executable_path,
-															)
-															.map(ToString::to_string)
-														})
-														.unwrap_or_else(|error| {
-															error!("Failed to get game name from exe path: {}", error);
-															"[Name Not Found]".to_string()
-														});
+												let display_name = app_packages
+													.open_subkey(&package_id)
+													.and_then(|package| {
+														package
+															.get_value::<String, _>("DisplayName")
+													})
+													.or_else(|error| {
+														error!("Failed to find display name for Xbox game: {}", error);
+														file_name_without_extension(
+															&executable_path,
+														)
+														.map(ToString::to_string)
+													})
+													.unwrap_or_else(|error| {
+														error!(
+														"Failed to get game name from exe path: {}",
+														error
+													);
+														"[Name Not Found]".to_string()
+													});
 
-													if let Some(game) = InstalledGame::new(
-														&executable_path,
-														&display_name,
-														*Self::ID,
-													) {
-														installed_callback(game.clone());
-													}
+												if let Some(game) = InstalledGame::new(
+													&executable_path,
+													&display_name,
+													*Xbox::ID,
+												) {
+													installed_callback(game.clone());
 												}
 											}
 										}
@@ -122,7 +140,7 @@ impl ProviderActions for Xbox {
 				}
 			}
 		}
-
-		Ok(())
 	}
+
+	Ok(())
 }
