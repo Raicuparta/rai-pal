807fb437b6ab1ca382650c38cb81aee7cb1e115a
Raicuparta <Raicuparta@users.noreply.github.com>
2024-09-23 23:14:50 +0200

    cleanup

--- backend/tauri-app/src/typescript.rs
+++ backend/tauri-app/src/typescript.rs
@@ -1,6 +1,7 @@
 #[cfg(debug_assertions)]
 use specta_typescript::{BigIntExportBehavior, Typescript};
 
+#[cfg(debug_assertions)]
 const BINDINGS_PATH: &str = "../../frontend/api/bindings.ts";
 
 pub fn export(builder: &tauri_specta::Builder<tauri::Wry>) {
