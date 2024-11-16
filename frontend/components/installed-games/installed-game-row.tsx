4c735a12c561961601e3658dee7b7fcfd727a51b
Raicuparta <Raicuparta@users.noreply.github.com>
2024-11-16 17:45:21 +0100

    properly rendering

--- frontend/components/installed-games/installed-game-row.tsx
+++ frontend/components/installed-games/installed-game-row.tsx
@@ -2,9 +2,7 @@ import { commands, InstalledGame, ProviderId } from "@api/bindings";
 import { Table } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { installedGamesColumns } from "./installed-games-columns";
-import { useTableRowContent } from "@components/table/use-table-row-content";
-import { ThumbnailCell } from "@components/table/thumbnail-cell";
-import { getThumbnailWithFallback } from "@util/fallback-thumbnail";
+import React from "react";
 
 type Props = {
 	readonly game_id: string;
@@ -13,8 +11,6 @@ type Props = {
 
 export function InstalledGameRow(props: Props) {
 	const [game, setGame] = useState<InstalledGame>();
-
-	const itemContent = useTableRowContent(installedGamesColumns);
 
 	useEffect(() => {
 		commands
@@ -26,29 +22,17 @@ export function InstalledGameRow(props: Props) {
 			});
 	}, [props.game_id, props.provider_id]);
 
-	if (!game) {
-		return (
-			<>
-				<Table.Td></Table.Td>
-				<Table.Td>...</Table.Td>
-			</>
-		);
-	}
-
 	return (
 		<>
-			<ThumbnailCell
-				src={getThumbnailWithFallback(game.thumbnailUrl, props.provider_id)}
-			/>
-			<Table.Td>{game.title.display}</Table.Td>
-			<Table.Td></Table.Td>
-			<Table.Td>{game.provider}</Table.Td>
-			<Table.Td>{game.executable.architecture}</Table.Td>
-			<Table.Td>{game.executable.scriptingBackend}</Table.Td>
-			<Table.Td>
-				{game.executable.engine?.brand}{" "}
-				{game.executable.engine?.version?.display}
-			</Table.Td>
+			{game ? (
+				installedGamesColumns.map((column) => (
+					<React.Fragment key={column.id}>
+						{column.renderCell(game)}
+					</React.Fragment>
+				))
+			) : (
+				<Table.Td>...</Table.Td>
+			)}
 		</>
 	);
 }
