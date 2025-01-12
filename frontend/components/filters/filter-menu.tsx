db0c50ea318d862d0c0ba5d14b166c72467b754f
Raicuparta <Raicuparta@users.noreply.github.com>
2025-01-12 15:46:53 +0100

    bring back the filter reset button

--- frontend/components/filters/filter-menu.tsx
+++ frontend/components/filters/filter-menu.tsx
@@ -2,7 +2,7 @@ import { Button, Group, Indicator, Popover } from "@mantine/core";
 import { IconFilter, IconX } from "@tabler/icons-react";
 import styles from "./filters.module.css";
 import { useCallback } from "react";
-import { FilterKey, FilterSelect, FilterValue } from "./filter-select";
+import { FilterChangeCallback, FilterKey, FilterSelect } from "./filter-select";
 import { SearchInput } from "@components/search-input";
 import { GamesFilter, GamesQuery } from "@api/bindings";
 import { defaultQuery, useDataQuery } from "@hooks/use-data-query";
@@ -10,23 +10,12 @@ import { defaultQuery, useDataQuery } from "@hooks/use-data-query";
 export function FilterMenu() {
 	const [dataQuery, setDataQuery] = useDataQuery();
 
-	const handleToggleClick = useCallback(
-		function <TFilterKey extends FilterKey>(
-			id: TFilterKey,
-			value: string | null,
-		) {
-			const newFilter = [...dataQuery.filter[id]];
-			const index = newFilter.indexOf(value as FilterValue<TFilterKey>);
-			if (index === -1) {
-				newFilter.push(value as FilterValue<TFilterKey>);
-			} else {
-				newFilter.splice(index, 1);
-			}
-
+	const handleToggleClick = useCallback<FilterChangeCallback>(
+		(id, values) => {
 			setDataQuery({
 				filter: {
 					...dataQuery?.filter,
-					[id]: newFilter,
+					[id]: values,
 				},
 			} as GamesQuery);
 		},
@@ -99,7 +88,7 @@ export function FilterMenu() {
 											id={filterId}
 											possibleValues={defaultQuery.filter[filterId]}
 											currentValues={dataQuery.filter[filterId]}
-											onClick={handleToggleClick}
+											onChange={handleToggleClick}
 										/>
 									),
 								)}
--- frontend/components/filters/filter-select.tsx
+++ frontend/components/filters/filter-select.tsx
@@ -1,16 +1,22 @@
 import { Button, Stack } from "@mantine/core";
 import { FilterButton } from "./filter-button";
 import { GamesFilter } from "@api/bindings";
+import { IconRestore } from "@tabler/icons-react";
 
 export type FilterKey = keyof GamesFilter;
 export type FilterValue<TFilterKey extends FilterKey> =
 	GamesFilter[TFilterKey][number];
 
+export type FilterChangeCallback = (
+	id: keyof GamesFilter,
+	value: Array<FilterValue<typeof id>>,
+) => void;
+
 type Props<TFilterKey extends keyof GamesFilter> = {
 	readonly id: TFilterKey;
 	readonly possibleValues: Array<FilterValue<TFilterKey>>;
 	readonly currentValues: Array<FilterValue<TFilterKey>>;
-	readonly onClick: (id: TFilterKey, value: FilterValue<TFilterKey>) => void;
+	readonly onChange: FilterChangeCallback;
 };
 
 type FilterDetails<TKey extends keyof GamesFilter> = {
@@ -59,8 +65,24 @@ export function FilterSelect<TFilterKey extends FilterKey>({
 	id,
 	possibleValues,
 	currentValues,
-	onClick,
+	onChange,
 }: Props<TFilterKey>) {
+	function handleFilterClick(id: TFilterKey, value: string | null) {
+		const newValues = [...currentValues];
+		const index = newValues.indexOf(value as FilterValue<TFilterKey>);
+		if (index === -1) {
+			newValues.push(value as FilterValue<TFilterKey>);
+		} else {
+			newValues.splice(index, 1);
+		}
+
+		onChange(id, newValues);
+	}
+
+	function handleResetClick() {
+		onChange(id, possibleValues);
+	}
+
 	return (
 		<Stack gap="xs">
 			<Button.Group orientation="vertical">
@@ -68,21 +90,20 @@ export function FilterSelect<TFilterKey extends FilterKey>({
 				{possibleValues.map((possibleValue) => (
 					<FilterButton
 						filterOption={possibleValue ?? filterDetails[id].emptyOption}
-						onClick={() => onClick(id, possibleValue)}
+						onClick={() => handleFilterClick(id, possibleValue)}
 						isVisible={currentValues.includes(possibleValue)}
 						note={filterDetails[id].valueNotes?.[possibleValue]}
 						key={possibleValue}
 					/>
 				))}
 			</Button.Group>
-			{/* <Button
-						onClick={handleReset}
-						leftSection={<IconRestore fontSize={10} />}
-						disabled={(hiddenValues?.length || 0) === 0}
-					>
-						Reset
-					</Button> */}
-			{/* TODO: reset button */}
+			<Button
+				onClick={handleResetClick}
+				leftSection={<IconRestore fontSize={10} />}
+				disabled={(currentValues?.length || 0) === possibleValues.length}
+			>
+				Reset
+			</Button>
 		</Stack>
 	);
 }
