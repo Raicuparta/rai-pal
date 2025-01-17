0bc5ba3c4761f0cea246a339101591c09e90d6aa
Raicuparta <Raicuparta@users.noreply.github.com>
2025-01-17 12:15:00 +0100

    Fix vite watch ignores

--- vite.config.ts
+++ vite.config.ts
@@ -30,8 +30,12 @@ export default defineConfig(async () => ({
 		// tauri.conf.json needs to specify the same port, under build.devUrl.
 		port: 1420,
 		watch: {
-			// The target folder has all the backend builds, and gets huge, making the watcher super slow.
-			ignored: ["backend/**"],
+			// Ignoring the backend folder helps with avoiding super slow startup especially on Windows.
+			// For some reason relative paths don't work at all here.
+			// Most solutions online solved this by passing '**/backend/**' here,
+			// but that would mean ignoring any folder with that name, anywhere on the project.
+			// So I'm passing the absolute path directly instead by using process.cwd().
+			ignored: [`${process.cwd()}/backend/**`],
 		},
 	},
 	build: {
