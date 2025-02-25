df050c87bd18507c2d5724dbb64112eb4f29c641
Raicuparta <Raicuparta@users.noreply.github.com>
2025-02-25 22:08:49 +0100

    remove helvetica from fonts so linux doesnt look weird

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -3,6 +3,11 @@ import { type MantineThemeOverride } from "@mantine/core";
 export const theme: MantineThemeOverride = {
 	defaultRadius: "md",
 	primaryColor: "violet",
+	fontFamily:
+		// Mantine comes with this long-ass default value for fontFamily so that it picks the first good one installed in the system,
+		// but it used to include Helvetica. This made it fall back to a weird broken font on Linux,
+		// where all text was shifted upwards a bit, so here I just copied that default but with helvetica removed.
+		"-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji",
 	colors: {
 		dark: [
 			"#C1C2C5",
