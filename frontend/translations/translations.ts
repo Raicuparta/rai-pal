6154b1112e20e3aea1bf09a92a4422d4c2f65144
Raicuparta <Raicuparta@users.noreply.github.com>
2025-03-05 02:09:07 +0100

    insane type that checks all params exist

--- frontend/translations/translations.ts
+++ frontend/translations/translations.ts
@@ -32,8 +32,20 @@ export type LanguageCode = keyof typeof translations;
 
 type BaseTranslation = typeof enUs;
 
+type ExtractParams<
+	T extends string
+> =
+	T extends `${string}{${infer Key}}${infer Rest}`
+		? [Key, ...ExtractParams<Rest>]
+		: []
+
+type StringWithParamsFromTuple<TTuple extends string[]> =
+  TTuple extends [infer K extends string, ...infer Rest extends string[]]
+    ? `${string}{${K}}${string}` & StringWithParamsFromTuple<Rest>
+    : string;
+
 export type Translation = {
 	[category in keyof BaseTranslation]: {
-		[translationKey in keyof BaseTranslation[category]]: string;
+		[translationKey in keyof BaseTranslation[category]]: StringWithParamsFromTuple<ExtractParams<BaseTranslation[category][translationKey] & string>>;
 	};
 };
