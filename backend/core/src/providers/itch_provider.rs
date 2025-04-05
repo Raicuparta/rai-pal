60a7626e43b13dcc9fb46eea1a1620ca10349279
Raicuparta <Raicuparta@users.noreply.github.com>
2025-04-05 18:03:22 +0200

    itch

--- backend/core/src/providers/itch_provider.rs
+++ backend/core/src/providers/itch_provider.rs
@@ -10,7 +10,8 @@ use sqlx::{Row, sqlite::SqlitePool};
 
 use super::provider_command::{ProviderCommand, ProviderCommandAction};
 use crate::{
-	game::{DbGame, GameId},
+	game::{DbGame, InsertGame},
+	game_executable::GameExecutable,
 	providers::provider::{ProviderActions, ProviderId, ProviderStatic},
 	result::{Error, Result},
 };
@@ -19,43 +20,36 @@ use crate::{
 pub struct Itch {}
 
 impl Itch {
-	fn get_installed_game(cave: &ItchDatabaseCave) -> Option<DbGame> {
-		// let verdict = cave.verdict.as_ref()?;
-		// let exe_path = verdict.base_path.join(&verdict.candidates.first()?.path);
-		// InstalledGame::new(&exe_path)
-		None
+	fn get_installed_game(cave: &ItchDatabaseCave) -> Option<GameExecutable> {
+		let verdict = cave.verdict.as_ref()?;
+		let exe_path = verdict.base_path.join(&verdict.candidates.first()?.path);
+		GameExecutable::new(&exe_path)
 	}
 
-	// fn get_game(row: &ItchDatabaseGame) -> DbGame {
-	// 	let mut game = Game::new(
-	// 		GameId {
-	// 			game_id: row.id.to_string(),
-	// 			provider_id: *Self::ID,
-	// 		},
-	// 		&row.title,
-	// 	);
+	fn get_game(row: &ItchDatabaseGame) -> DbGame {
+		let mut game = DbGame::new(*Self::ID, row.id.to_string(), row.title.clone());
 
-	// 	if let Some(thumbnail_url) = &row.cover_url {
-	// 		game.set_thumbnail_url(thumbnail_url);
-	// 	}
-	// 	if let Some(date_time) = row
-	// 		.published_at
-	// 		.as_ref()
-	// 		.and_then(|published_at| DateTime::parse_from_rfc3339(published_at).ok())
-	// 	{
-	// 		game.set_release_date(date_time.timestamp());
-	// 	}
-	// 	game.add_provider_command(
-	// 		ProviderCommandAction::ShowInLibrary,
-	// 		ProviderCommand::String(format!("itch://games/{}", row.id)),
-	// 	)
-	// 	.add_provider_command(
-	// 		ProviderCommandAction::Install,
-	// 		ProviderCommand::String(format!("itch://install?game_id={}", row.id)),
-	// 	);
+		game.thumbnail_url = row.cover_url.clone();
 
-	// 	game
-	// }
+		if let Some(date_time) = row
+			.published_at
+			.as_ref()
+			.and_then(|published_at| DateTime::parse_from_rfc3339(published_at).ok())
+		{
+			game.release_date = Some(date_time.timestamp());
+		}
+
+		game.add_provider_command(
+			ProviderCommandAction::ShowInLibrary,
+			ProviderCommand::String(format!("itch://games/{}", row.id)),
+		)
+		.add_provider_command(
+			ProviderCommandAction::Install,
+			ProviderCommand::String(format!("itch://install?game_id={}", row.id)),
+		);
+
+		game
+	}
 }
 
 impl ProviderStatic for Itch {
@@ -105,6 +99,34 @@ pub struct ItchDatabase {
 
 impl ProviderActions for Itch {
 	async fn insert_games(&self, pool: &sqlx::Pool<sqlx::Sqlite>) -> Result {
+		let app_data_path = directories::BaseDirs::new()
+			.ok_or_else(Error::AppDataNotFound)?
+			.config_dir()
+			.join("itch");
+
+		if let Some(database) = get_database(&app_data_path).await? {
+			let caves_map: HashMap<_, _> = database
+				.caves
+				.into_iter()
+				.map(|cave| (cave.id, cave))
+				.collect();
+
+			for db_entry in database.games {
+				let mut game = Self::get_game(&db_entry);
+				if let Some(executable) = caves_map
+					.get(&db_entry.id)
+					.and_then(Self::get_installed_game)
+				{
+					game.set_executable(&executable);
+				}
+				pool.insert_game(&game).await?; // TODO don't crash whole thing if single game fails
+			}
+		} else {
+			log::info!(
+				"Itch database file not found. Probably means user hasn't installed the Itch app."
+			);
+		}
+
 		Ok(())
 	}
 
@@ -112,31 +134,6 @@ impl ProviderActions for Itch {
 	where
 		TCallback: FnMut(DbGame) + Send + Sync,
 	{
-		// let app_data_path = directories::BaseDirs::new()
-		// 	.ok_or_else(Error::AppDataNotFound)?
-		// 	.config_dir()
-		// 	.join("itch");
-
-		// if let Some(database) = get_database(&app_data_path).await? {
-		// 	let caves_map: HashMap<_, _> = database
-		// 		.caves
-		// 		.into_iter()
-		// 		.map(|cave| (cave.id, cave))
-		// 		.collect();
-
-		// 	for db_entry in database.games {
-		// 		let mut game = Self::get_game(&db_entry);
-		// 		if let Some(cave) = caves_map.get(&db_entry.id) {
-		// 			game.installed_game = Self::get_installed_game(cave);
-		// 		}
-		// 		callback(game);
-		// 	}
-		// } else {
-		// 	log::info!(
-		// 		"Itch database file not found. Probably means user hasn't installed the Itch app."
-		// 	);
-		// }
-
 		Ok(())
 	}
 }
