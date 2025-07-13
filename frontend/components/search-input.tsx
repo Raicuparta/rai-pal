5642771f3b286f28a7d08312b5689f3b50d877ed
Raicuparta <Raicuparta@users.noreply.github.com>
2025-07-13 14:51:08 +0200

    debounce search input

--- frontend/components/search-input.tsx
+++ frontend/components/search-input.tsx
@@ -1,5 +1,7 @@
 import { useLocalization } from "@hooks/use-localization";
 import { CloseButton, Input } from "@mantine/core";
+import { useDebouncedCallback } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 
 type Props = {
 	readonly onChange: (search: string) => void;
@@ -8,19 +10,32 @@ type Props = {
 
 export function SearchInput(props: Props) {
 	const t = useLocalization("filterMenu");
+	const [innerValue, setInnerValue] = useState(props.value);
 
-	// TODO on linux it seems to be losing key presses whe typing fast
+	useEffect(() => {
+		setInnerValue(props.value);
+	}, [props.value]);
+
+	const debouncedOnChange = useDebouncedCallback(props.onChange, 200);
+
+	const setValue = (value: string) => {
+		setInnerValue(value);
+		debouncedOnChange(value);
+	};
+
 	return (
 		<Input
-			onChange={(event) => props.onChange(event.target.value)}
+			onChange={(event) => {
+				setValue(event.currentTarget.value);
+			}}
 			placeholder={t("searchPlaceholder")}
 			style={{ flex: 1 }}
-			value={props.value}
+			value={innerValue}
 			rightSectionPointerEvents="all"
 			rightSection={
 				<CloseButton
-					onClick={() => props.onChange("")}
-					style={{ display: props.value ? undefined : "none" }}
+					onClick={() => setValue("")}
+					style={{ display: innerValue ? undefined : "none" }}
 				/>
 			}
 		/>
