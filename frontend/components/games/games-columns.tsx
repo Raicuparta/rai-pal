870a7cc997ca070c3c7058f31f22227773479fee
Raicuparta <Raicuparta@users.noreply.github.com>
2026-04-08 14:55:59 +0200

    Revert "more worse colors"
    This reverts commit 77d7d910b2791e19f0c5b263b39a56e9e70c6d27.

--- frontend/components/games/games-columns.tsx
+++ frontend/components/games/games-columns.tsx
@@ -55,19 +55,18 @@ const status: GamesColumn = {
 	component: ({ item }: CellProps) => (
 		<Table.Td
 			p="xs"
+			bg={`var(--mantine-color-${providerColors[item.providerId]}-light)`}
 			opacity={item.exePath ? 1 : 0.5}
 		>
 			<Stack
 				justify="center"
 				align="center"
-				c={providerColors[item.providerId]}
 			>
-				<ProviderIcon providerId={item.providerId} />
-				{item.exePath ? (
-					<IconDeviceDesktop color="white" />
-				) : (
-					<IconCloud color="gray" />
-				)}
+				<ProviderIcon
+					providerId={item.providerId}
+					color={`var(--mantine-color-${providerColors[item.providerId]}-light-color)`}
+				/>
+				{item.exePath ? <IconDeviceDesktop color="white" /> : <IconCloud />}
 			</Stack>
 		</Table.Td>
 	),
