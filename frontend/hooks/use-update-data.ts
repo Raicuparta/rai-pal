b1f6d06dd644ae042aca316781616a82c6792bfd
Raicuparta <Raicuparta@users.noreply.github.com>
2026-06-04 20:01:30 +0200

    refactor command atom data

--- frontend/hooks/use-update-data.ts
+++ frontend/hooks/use-update-data.ts
@@ -6,68 +6,33 @@ import { showAppNotification } from "@components/app-notifications";
 import { useAppEvent } from "./use-app-event";
 import { useThrottledCallback } from "@mantine/hooks";
 import { useDataQuery } from "./use-data-query";
+import { useCommandAtomData } from "./use-command-data";
 
-// TODO this is kinda stupid, hook doing too much work since it's used in multiple places.
 export function useUpdateData(executeOnMount = false) {
 	const setLoadingTasks = useSetAtom(loadingTasksAtom);
-	const setGameData = useSetAtom(gameDataAtom);
-	const setMods = useSetAtom(modsAtom);
 	const [gamesQuery] = useDataQuery();
-	const totalGameFetchCount = useRef(0);
-	const totalModFetchCount = useRef(0);
 	const totalLoadingTaskCount = useRef(0);
 	const hasExecutedOnMount = useRef(false);
 
-	const updateGames = useCallback(() => {
-		totalGameFetchCount.current++;
-		const thisFetchCount = totalGameFetchCount.current;
-		commands
-			.getGameIds(gamesQuery)
-			.then((data) => {
-				if (thisFetchCount !== totalGameFetchCount.current) {
-					console.log(
-						"Cancelling this fetch since another one happened in the meantime.",
-					);
-					return;
-				}
+	const getGameIds = useCallback(
+		() => commands.getGameIds(gamesQuery),
+		[gamesQuery],
+	);
 
-				setGameData(data);
-			})
-			.catch((error) => {
-				showAppNotification(`Failed to get app games data: ${error}`, "error");
-			});
-	}, [gamesQuery, setGameData]);
+	const updateGames = useCommandAtomData(getGameIds, gameDataAtom);
 
-	const updateMods = useCallback(() => {
-		totalModFetchCount.current++;
-		const thisFetchCount = totalModFetchCount.current;
-		commands
-			.getMods()
-			.then((data) => {
-				if (thisFetchCount !== totalModFetchCount.current) {
-					console.log(
-						"Cancelling this fetch since another one happened in the meantime.",
-					);
-					return;
-				}
-
-				setMods(data);
-			})
-			.catch((error) => {
-				showAppNotification(`Failed to get app mods data: ${error}`, "error");
-			});
-	}, [setMods]);
-
-	const throttledUpdateData = useThrottledCallback(() => {
-		updateGames();
-		updateMods();
-	}, 1000);
+	const updateMods = useCommandAtomData(commands.getMods, modsAtom);
 
 	useEffect(() => {
 		if (!executeOnMount) return;
 		updateGames();
 		updateMods();
-	}, [updateGames, executeOnMount, updateMods]);
+	}, [executeOnMount, updateGames, updateMods]);
+
+	const throttledUpdateData = useThrottledCallback(() => {
+		updateGames();
+		updateMods();
+	}, 1000);
 
 	useAppEvent("appDatabaseChanged", "update-data", throttledUpdateData);
 
